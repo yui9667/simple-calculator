cec87d9ce1d88326583831178cc212eace9c835c
Yui Jensen <kyanyui67@gmail.com>
2024-04-02 13:48:00 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const num1 = document.getElementById("number1");
+const num2 = document.getElementById("number2");
+const btn = document.getElementById("btn");
+const result = document.getElementById("result");
+function sum() {
+  btn.addEventListener("click", {
+    if() {
+      return num1 + num2;
+    },
+  });
+}
